8d381cf4b565a0d92613decf7f7404279f229375
antoniopelaezmoreno <antpelmor@alum.us.es>
2023-03-06 18:11:35 +0100

    Creado rol Lecturer

---
+++ src/main/java/acme/roles/Lecturer.java
@@ -0,0 +1,29 @@
+
+package acme.roles;
+
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractRole;
+
+public class Lecturer extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			almaMater;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			resume;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			qualifications;
+
+	@URL
+	protected String			furtherInformation;
+}

6578c8b0d18170d903a8a6e4b7949e78a18739cb
antoniopelaezmoreno <antpelmor@alum.us.es>
2023-03-07 11:20:25 +0100

    Añadidos @Entity, @Getter y @Setter a Lecturer

--- src/main/java/acme/roles/Lecturer.java
+++ src/main/java/acme/roles/Lecturer.java
@@ -1,13 +1,19 @@
 
 package acme.roles;
 
+import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Lecturer extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;

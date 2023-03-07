c6b276999181a640f512df47dd7a4205298a84ef
antoniopelaezmoreno <antpelmor@alum.us.es>
2023-03-07 12:13:13 +0100

    Modificada entidad Note

--- src/main/java/acme/entities/note/Note.java
+++ src/main/java/acme/entities/note/Note.java
@@ -1,12 +1,11 @@
 
 package acme.entities.note;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Past;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
@@ -25,23 +24,22 @@ public class Note extends AbstractEntity {
 	 */
 	private static final long	serialVersionUID	= 1L;
 
-	@Past
-	private LocalDateTime		instantiationMoment;
+	private Date				instantiationMoment;
 
 	@NotBlank
-	@Length(max = 76)
+	@Length(max = 75)
 	private String				title;
 
 	@NotBlank
-	@Length(max = 76)
+	@Length(max = 75)
 	private String				author;
 
 	@NotBlank
-	@Length(max = 101)
+	@Length(max = 100)
 	private String				message;
 
 	@Email
-	private String				email;
+	private String				emailAddress;
 
 	@URL
 	private String				link;

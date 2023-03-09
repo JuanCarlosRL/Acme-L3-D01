56bdd4d63cc71f4daf635cf9da296fe6afe068b0
alexsm01 <alejsm01@gmail.com>
2023-03-09 03:07:16 +0100

    Entidad enrolment creada

--- src/main/java/acme/entities/workbook/Workbook.java
+++ src/main/java/acme/entities/workbook/Workbook.java
@@ -4,6 +4,7 @@ package acme.entities.workbook;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
@@ -12,7 +13,7 @@ import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
-// import acme.entities.enrolment.Enrolment;
+import acme.entities.enrolment.Enrolment;
 import acme.enums.Indication;
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
@@ -47,7 +48,9 @@ public class Workbook extends AbstractEntity {
 	@URL
 	protected String			link;
 
-	//@ManyToOne(optional = false)
-	//protected Enrolment			enrolment;
+	// Relationships
+
+	@ManyToOne(optional = false)
+	protected Enrolment			enrolment;
 
 }

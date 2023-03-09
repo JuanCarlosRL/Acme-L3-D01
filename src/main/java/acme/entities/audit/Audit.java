51d9a4a574ece11a5e43c60d706b3389115157ec
Jaime García García <jaigargar1@alum.us.es>
2023-03-09 02:33:14 +0100

    Audit entity and Mark creation. UML update

--- src/main/java/acme/entities/audit/Audit.java
+++ src/main/java/acme/entities/audit/Audit.java
@@ -1,16 +1,19 @@
 
-package acme.entities.lecture;
+package acme.entities.audit;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.enums.Mark;
 import acme.framework.data.AbstractEntity;
 import acme.roles.Auditor;
 import lombok.Getter;
@@ -19,20 +22,21 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Lecture extends AbstractEntity {
+public class Audit extends AbstractEntity {
 
 	/**
 	 * 
 	 */
 	private static final long	serialVersionUID	= 1L;
 
+	@Pattern(regexp = "“[A-Z]{1,3} [0-9]{3}")
 	@NotBlank
-	@Length(max = 75)
-	protected String			title;
+	@Column(unique = true)
+	protected String			code;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String			lectureAbstract;
+	protected String			conclusion;
 
 	@Positive
 	@NotNull
@@ -40,15 +44,17 @@ public class Lecture extends AbstractEntity {
 
 	@NotBlank
 	@Length(max = 100)
-	protected String			body;
+	protected String			strongPoints;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			weakPoints;
 
 	@NotNull
-	protected Indication		indicator;
+	protected Mark				mark;
 
 	@URL
 	protected String			link;
-
-	protected boolean			publicado;
 
 	@NotNull
 	@Valid

06aac98dba49fcdd06d1391c9cc6b88329d676dc
Jaime García García <jaigargar1@alum.us.es>
2023-03-10 01:05:10 +0100

    Diagram fixes

--- src/main/java/acme/entities/tutorial/Tutorial.java
+++ src/main/java/acme/entities/tutorial/Tutorial.java
@@ -44,10 +44,7 @@ public class Tutorial extends AbstractEntity {
 	protected String			goals;
 
 	@Positive
-<<<<<<< HEAD
 	protected int				estimatedTime;
-=======
-	protected double			estimatedTime;
 
 	@NotNull
 	@Valid
@@ -58,6 +55,5 @@ public class Tutorial extends AbstractEntity {
 	@Valid
 	@ManyToOne(optional = false)
 	protected Assistant			assistant;
->>>>>>> branch 'master' of https://github.com/frafermot/Acme-L3-D02.git
 
 }

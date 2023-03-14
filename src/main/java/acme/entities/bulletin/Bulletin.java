a486f393033d8eea4b3399183e89ced7ef8656d4
Jaime García García <jaigargar1@alum.us.es>
2023-03-14 01:05:54 +0100

    Bulletin entity fix

--- src/main/java/acme/entities/bulletin/Bulletin.java
+++ src/main/java/acme/entities/bulletin/Bulletin.java
@@ -1,11 +1,11 @@
 
 package acme.entities.bulletin;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -13,7 +13,6 @@ import javax.validation.constraints.Past;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
-import acme.framework.components.accounts.Administrator;
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,29 +24,22 @@ public class Bulletin extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
 
+	@NotNull
 	@Past
-	@Column(name = "instantiation_moment", unique = false, nullable = false)
-	private LocalDateTime		updateTime;
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				instantiationMoment;
 
 	@NotBlank
 	@Length(max = 75)
-	@Column(name = "title", unique = false)
-	private String				title;
+	protected String			title;
 
 	@NotBlank
 	@Length(max = 100)
-	@Column(name = "message", unique = false)
-	private String				message;
+	protected String			message;
 
-	@Column(name = "critical", unique = false)
-	private boolean				critical;
+	protected boolean			flag;
 
 	@URL
-	@Column(name = "link", unique = false, nullable = true)
 	private String				link;
-
-	@ManyToOne
-	@NotNull
-	private Administrator		admin;
 
 }
